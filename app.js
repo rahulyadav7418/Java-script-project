0f65eb60cb2e0cc653fe27cdcd24ad9a22c70584
rahulyadav7418 <yadavrahul776393@gmail.com>
2026-05-14 21:11:29 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,29 +1,124 @@
-let todo = [];
+// let btn = document.querySelector("button");
+// let inp = document.querySelector("input");
+// let ul = document.querySelector("ul");
 
-let req = prompt("please enter your request");
+// btn.addEventListener("click", function () {
+//     let item = document.createElement("li");
+//     item.innerText = inp.value;
+//     ul.appendChild(item);
 
-while(true) {
-    if(req == "quit") {
-        console.log("quitting app");
-            break;
-        }
+//     let delBtn = document.createElement("button");
+//     delBtn.innerText = "delete";
+//     delBtn.classList.add("delete");
 
-    if(req == "list") {
-        console.log("------------------");
-        for(let i=0; i<todo.length; i++) {
-            console.log(i, todo[i]);
-        }
-        console.log("------------------");
-    } else if(req == "add") {
-        let task = prompt("Please enter the task you want to add");
-        todo.push(task);
-        console.log("task added");
-    } else if(req == "delete") {
-        let idx = prompt("Please enter the task index");
-        todo.splice(idx, 1);
-    } else {
-        console.log("please wrong request");
+//     item.appendChild(delBtn);
+//     ul.appendChild(item);
+//     inp.value = "";
+
+// });
+
+// ul.addEventListener("click", function (event) {
+//     if (event.target.nodeName == "BUTTON") {
+//     let listItem = event.target.parentElement;
+//     listItem.remove();
+//     console.log("Deleted");
+//     }
+// })
+
+
+
+//Simon Says Game.
+
+let gameSeq = [];
+let userSeq = [];
+
+let btns = ["red", "yellow", "green", "purple"];
+
+let started = false;
+let level = 0;
+
+let h2 = document.querySelector("h2");
+
+document.addEventListener("keypress", function () {
+    if (started == false) {
+        console.log("Game is started");
+        started = true;
+        
+        levelUp();
     }
-    req = prompt("please enter your request");
+});
+
+
+function gameFlash(btn) {
+    btn.classList.add("flash");
+    setTimeout(function () {
+        btn.classList.remove("flash");
+    }, 250);
 }
-    
+
+function userFlash(btn) {
+    btn.classList.add("userflash");
+    setTimeout(function () {
+        btn.classList.remove("userflash");
+    }, 250);
+}
+
+
+function levelUp() {
+ level++;
+ h2.innerText = `Level ${level}`;
+
+ let randIdx = Math.floor(Math.random() * 3);
+ let randColor = btns[randIdx];
+ let randBtn = document.querySelector(`.${randColor}`);
+ 
+//  console.log(randIdx);
+//  console.log(randColor);
+//  console.log(randBtn);
+ gameSeq.push(randColor);
+ console.log(gameSeq);
+ gameFlash(randBtn);
+
+}
+
+function checkAns () {
+    // console.log("cur level :", level);
+    let idx = level - 1;
+    if (userSeq[idx] === gameSeq[idx]) {
+        if (userSeq[idx] === gameSeq[idx]) {
+            setTimeout(levelUp, 1000);
+        }
+    } else {
+        h2.innerHTML = `Game over! Your Score was <b>${level}</b> <br> Press any key to start game.`;
+        document.querySelector("body").style.backgroundColor = "red";
+        setTimeout(function () {
+            document.querySelector("body").style.backgroundColor = "white";
+        }, 150);
+        reset();
+    }
+}
+
+function btnPress () {
+    let btn = this;
+    userFlash(btn);
+
+    userColor = btn.getAttribute("id");
+    userSeq.push(userColor);
+
+
+    checkAns();
+}
+
+let allBtns = document.querySelectorAll(".btn");
+for (btn of allBtns) {
+    btn.addEventListener("click", btnPress);
+}
+
+function reset() {
+    started = false;
+    gameSeq = [];
+    userSeq = [];
+    level = 0;
+}
+
+
